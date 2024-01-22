a3cf5d88cc9d46a9a635cd096a3694621938e333
Nicolas <nicolasgfalabella@gmail.com>
2024-01-21 21:06:38 -0300

    Cambios del dia domingo

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -15,7 +15,7 @@ const TasksPage = () => {
     <div className="grid grid-cols-3 gap-2">
       {
         tasks.map( (task) => (
-        <TaskCard key = {task.id} task={task}/>
+        <TaskCard key = {task._id} task={task}/>
         ))
           
         
